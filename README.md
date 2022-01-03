31edaca865d49f66693b259aa4f27fb6656e281b
Alan Willoughby <62529956+satyadvaya@users.noreply.github.com>
2022-01-03 11:27:29 -0800

    Initialize navigable

---
+++ README.md
@@ -0,0 +1,5 @@
+# Secret Treehouse
+
+A demo of private routes in React Router (no backend).
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/f62dbd79-96d6-4ca9-bcae-079d68099b66/deploy-status)](https://app.netlify.com/sites/alchemy-react-secret-treehouse/deploys)
---
+++ src/views/Auth/Login.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
+import { useForm } from '../../hooks/useForm';
+import styles from './Login.css';
+
+export default function Login() {
+  const history = useHistory();
+  const location = useLocation();
+  const auth = useAuth();
+  const { formState, handleFormChange } = useForm({ email: '', password: '' });
+  const [error, setError] = useState(null);
+
+  const { from } = location.state || { from: { pathname: '/' } };
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const loginWasSuccessful = auth.login(formState.email, formState.password);
+    if (loginWasSuccessful) {
+      history.replace(from);
+    } else {
+      setError(
+        'Sign in unsuccessful. Please check your credentials & try again.'
+      );
+    }
+  };
+
+  return (
+    <>
+      <h3>You must log in to view the page at {from.pathname}</h3>
+      <form onSubmit={handleLogin} className={styles.loginForm}>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          value={formState.email}
+          onChange={handleFormChange}
+        />{' '}
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          value={formState.password}
+          onChange={handleFormChange}
+        />
+        <button type="submit" aria-label="Sign In">
+          Sign in
+        </button>
+      </form>
+      {error && <h4 className={styles.error}>{error}</h4>}
+    </>
+  );
+}
